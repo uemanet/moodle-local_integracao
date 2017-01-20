892a56814867dfead7adc0960b76e6492dab1ea9
Pedro Fellipe <pedro_fellipe123@hotmail.com>
2017-01-20 17:56:22 -0300

    funções bases do web service criadas

---
+++ lang/en/local_integracao.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * 
+ *
+ * @package monitor/lang
+ * @copyright 2016 Uemanet
+ */
+
+$string['pluginname'] = 'Integracao';
---
+++ version.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @package integracao
+ * @copyright 2016 Uemanet
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2017012002;
+$plugin->requires = 2013062000;
+$plugin->release = '0.1.1';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->component = 'local_integracao';
+
+/*$plugin->dependencies = array(
+    'local_integracao' => ANY_VERSION
+);*/
