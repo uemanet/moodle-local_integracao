541c7f30c17f48292b3d1b7713414c84d6c089f4
PedroFellipe <pedrofellipemelo123@gmail.com>
2017-01-26 12:46:31 -0300

    retirado moodle_exception em group

--- lang/en/local_integracao.php
+++ lang/en/local_integracao.php
@@ -12,4 +12,3 @@ $string['pluginname'] = 'Integracao';
 
 // Errors strings
 $string['databaseaccesserror'] = 'Error on trying access database';
-$string['nocoursegrouperror'] = 'Nenhum curso mapeado com a turma com trm_id:';
