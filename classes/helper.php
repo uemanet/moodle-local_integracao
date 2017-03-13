d10fe837e23e264de49f75386e9dad3f92e73878
PedroFellipe <pedrofellipemelo123@gmail.com>
2017-03-13 19:55:33 -0300

    bloqueio sendo feito de acordo com os papeis definidos pelo plugin

--- classes/helper.php
+++ classes/helper.php
@@ -1,29 +1,104 @@
 <?php
-
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 /**
- * Class helper
+ * Miidle Web Service - helper manager
  *
- * @package classes
- * @copyright 2017 Uemanet
+ * @package    contrib
+ * @subpackage local_integracao
+ * @copyright  2016 Willian Mano http://willianmano.github.io
+ * @authors    Willian Mano
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-abstract class helper
-{
-    const FILE = "integracao.json";
-
-    public static function debug()
+defined('MOODLE_INTERNAL') || die();
+class local_integracao_helper {
+    public static $lookupeventswithoutredirecttime = array(
+        '\mod_assign\event\submission_status_viewed',
+        '\mod_assign\event\submission_form_viewed',
+        '\mod_resource\event\course_module_viewed',
+        '\mod_forum\event\course_module_viewed',
+    );
+    public static $lookupevents = array(
+        '\mod_page\event\course_module_viewed',
+        '\mod_folder\event\course_module_viewed',
+        '\mod_quiz\event\course_module_viewed',
+        '\mod_wiki\event\page_viewed',
+        '\mod_forum\event\discussion_viewed',
+        '\mod_quiz\event\attempt_viewed',
+        '\core\event\course_viewed'
+    );
+    /**
+     * Observe the events, and dispatch them if necessary.
+     *
+     * @param \core\event\base $event The event.
+     * @return void
+     */
+    public static function observer(\core\event\base $event)
     {
-        return helper::parse_config_file('debug');
+        if(self::isMonitoredEvent($event)) {
+            self::verifyEvent($event);
+        }
     }
 
-    private static function parse_config_file($key = '')
+    /**
+     *
+     * @param \core\event\base $event The event.
+     * @return boolean
+     */
+    protected static function isMonitoredEvent(\core\event\base $event)
     {
-        $fileContent = file_get_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR . helper::FILE);
-        $json = json_decode($fileContent, true);
-
-        if(array_key_exists($key, $json)){
-            return $json[$key];
+        if(in_array($event->eventname, array_merge(self::$lookupevents, self::$lookupeventswithoutredirecttime))) {
+            return true;
         }
+        if($event instanceof \core\event\course_module_viewed) {
+            return true;
+        }
+        return false;
+    }
+    /**
+     * Observe the events, and dispatch them if necessary.
+     *
+     * @param \core\event\base $event The event.
+     * @return void
+     */
+    protected static function verifyEvent(\core\event\base $event) {
+        if(is_siteadmin()) {
+            return;
+        }
+        // So executa as acoes de bloqueio caso seja aluno
+        $context = context_course::instance($event->courseid);
+        $userRoles = get_user_roles($context, $event->userid);
+        $isStudent = false;
 
-        return null;
+        // pega as configuraçoes do plugin
+        $config = get_config('local_integracao');
+
+        $aluno = $config->aluno;
+        $alunoconcluido = $config->aluno_concluido;
+        $alunoreprovado = $config->aluno_reprovado;
+        $alunotrancado = $config->aluno_trancado;
+
+        if(!empty($userRoles)) {
+            foreach ($userRoles as $r => $role) {
+                if($role->roleid == $aluno || $role->roleid == $aluno_concluido || $role->roleid == $aluno_reprovado || $role->roleid == $aluno_trancado ) {
+                    $isStudent = true;
+                    break;
+                }
+            }
+        }
+        if ($isStudent) {
+            $manager = local_integracao_manager::get($event->courseid);
+            $manager->capture_event($event);
+        }
     }
 }
---
+++ classes/manager.php
@@ -0,0 +1,135 @@
+<?php
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Miidle Web Service - manager
+ *
+ * @package    contrib
+ * @subpackage local_integracao
+ * @copyright  2016 Willian Mano http://willianmano.github.io
+ * @authors    Willian Mano
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+class local_integracao_manager {
+    /** @var array Array of singletons. */
+    protected static $instances;
+    /** @var int Course ID. */
+    protected $courseid = null;
+    protected $course = null;
+    /**
+     * Constructor
+     *
+     * @param int $courseid The course ID.
+     * @return void
+     */
+    protected function __construct($courseid) {
+        global $DB;
+        $this->courseid = $courseid;
+        $this->course = $DB->get_record('course', array('id'=>$courseid), '*', MUST_EXIST);
+    }
+    /**
+     * Capture an event.
+     *
+     * @param \core\event\base $event The event.
+     * @return void
+     */
+    public function capture_event(\core\event\base $event) {
+        global $DB, $CFG;
+        $redirecttime = 0;
+        if ($event->courseid !== $this->courseid) {
+            throw new coding_exception('Event course ID does not match event course ID');
+        }
+        // The capture has not been enabled yet.
+        if (!$this->is_enabled($event->userid)) {
+            return;
+        }
+        if ($event instanceof \core\event\course_viewed && $event->contextlevel == 50
+            && ($event->contextinstanceid == $event->courseid)) {
+            return;
+        }
+        if($event instanceof \core\event\course_viewed && isset($event->other['coursesectionnumber'])) {
+            $sql = "SELECT * FROM {course_sections} cs
+                    INNER JOIN {int_discipline_section} dc ON dc.sectionid = cs.id
+                    INNER JOIN {int_user_discipline} ud ON ud.sectionid = cs.id AND ud.userid = :userid
+                    WHERE
+                      cs.course = :courseid
+                      AND cs.section = :sectionnumber";
+            $params['userid'] = $event->userid;
+            $params['courseid'] = $event->contextinstanceid;
+            $params['sectionnumber'] = $event->other['coursesectionnumber'];
+
+            $section = $DB->get_record_sql($sql, $params);
+
+            if($section) {
+                return;
+            }
+            redirect(new moodle_url('/course/view.php',
+                    array('id' => $this->courseid)), 'Você não esta matriculado nesta disciplina.', $redirecttime);
+        }
+        // Verifica se a atividade esta em uma section mapeada
+        $sql = "SELECT cs.id as sectionid
+                FROM {course_modules} cm
+                INNER JOIN {course_sections} cs ON cm.section = cs.id
+                INNER JOIN {int_discipline_section} dc ON dc.sectionid = cs.id
+                WHERE cm.id = :instanceid";
+        $params['instanceid'] = $event->contextinstanceid;
+        $section = $DB->get_record_sql($sql, $params);
+        if(!$section) {
+            return;
+        }
+        // Verifica se o aluno esta matriculado na disciplina
+        $isEnrolled = $DB->count_records('int_user_discipline', array('sectionid' => $section->sectionid, 'userid' => $event->userid));
+        if(!$isEnrolled) {
+            if(!in_array($event->eventname, local_integracao_helper::$lookupeventswithoutredirecttime)) {
+                $redirecttime = 5;
+            }
+            redirect(new moodle_url('/course/view.php',
+                    array('id' => $this->courseid)), 'Você não esta matriculado na disciplina desta atividade.',$redirecttime);
+        }
+    }
+    /**
+     * Get an instance of the manager.
+     *
+     * @param int $courseid The course ID.
+     * @param bool $forcereload Force the reload of the singleton, to invalidate local cache.
+     * @return local_integracao_manager The instance of the manager.
+     */
+    public static function get($courseid, $forcereload = false) {
+        if ($forcereload || !isset(self::$instances[$courseid])) {
+            self::$instances[$courseid] = new local_integracao_manager($courseid);
+        }
+        return self::$instances[$courseid];
+    }
+    /**
+     * Return the current course ID.
+     *
+     * @return int The course ID.
+     */
+    public function get_courseid() {
+        return $this->courseid;
+    }
+    /**
+     * Is the block enabled on the course?
+     *
+     * @return boolean True if enabled.
+     */
+    public function is_enabled($userid) {
+        global $DB;
+        $isIntegrado = $DB->count_records('int_turma_course', array('courseid' => $this->get_courseid()));
+        if($isIntegrado) {
+            return true;
+        }
+        return false;
+    }
+}
